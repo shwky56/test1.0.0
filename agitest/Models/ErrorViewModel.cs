f9e82d4fd12bbdd44e8756e7cb84c273d9ce202c
shwky <shwkym54@gmil.com>
2024-01-21 16:10:05 +0200

    add

---
+++ agitest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace agitest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
